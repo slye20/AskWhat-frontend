35284b186f9bfb3d49f9a5ec957990f96ef627db
slye20 <samuellim2011@gmail.com>
2023-12-30 16:32:52 +0800

    Created thread creation page

--- src/pages/CreateNewThread.tsx
+++ src/pages/CreateNewThread.tsx
@@ -1,31 +1,60 @@
-import { Box, Button, TextField, Typography } from "@mui/material";
+import Category from "../types/Category";
+import {
+    Button,
+    Checkbox,
+    FormControl,
+    InputLabel,
+    ListItemText,
+    MenuItem,
+    OutlinedInput,
+    Select,
+    SelectChangeEvent,
+    TextField,
+    Typography,
+} from "@mui/material";
 
-import React, { ChangeEvent, FormEvent, useState } from "react";
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+
+type FormData = {
+    title: string;
+    text: string;
+    categories: string[];
+};
 
 const CreateThread: React.FC = () => {
-    const [formData, setFormData] = useState({ title: "", text: "", username: "" });
-    const [errors, setErrors] = useState({ title: false, username: false }); // error if content is empty
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [formData, setFormData] = useState<FormData>({ title: "", text: "", categories: [] });
+    const [errors, setErrors] = useState(""); // error if content is empty
+
+    useEffect(() => {
+        const url = `http://localhost:3000/categories`;
+        fetch(url)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                }
+                throw new Error("Network response was not ok.");
+            })
+            .then((t) => setCategories(t));
+    }, []);
 
     // When submit button is pressed, add thread, reroute to new thread
     const handleSubmit = (event: FormEvent) => {
         // prevent page from refreshing
         event.preventDefault();
         console.log(formData);
-        const { title, username } = formData;
-        const newErrors = { title: !title.trim(), username: !username.trim() };
-
-        if (newErrors.title || newErrors.username) {
-            setErrors(newErrors);
-            return;
-        }
-
-        setFormData({ ...formData, title: "", text: "" });
     };
 
     const handleChange = (field: string) => (event: ChangeEvent<HTMLInputElement>) => {
         setFormData({ ...formData, [field]: event.target.value });
-        setErrors({ ...errors, [field]: false });
+        setErrors("");
     };
+
+    const handleSelectChange = (event: SelectChangeEvent<string[]>) => {
+        setFormData({ ...formData, categories: event.target.value as string[] });
+    };
+
+    errors; //remove
 
     return (
         <div style={{ margin: "auto", textAlign: "center", width: "80vw" }}>
@@ -35,8 +64,6 @@ const CreateThread: React.FC = () => {
 
             <form onSubmit={(event) => handleSubmit(event)}>
                 <TextField
-                    error={errors.title}
-                    helperText={errors.title ? "This field cannot be empty." : ""}
                     onChange={handleChange("title")}
                     label="Title"
                     fullWidth={true}
@@ -56,28 +83,25 @@ const CreateThread: React.FC = () => {
                     value={formData.text}
                     sx={{ m: 1 }}
                 />
+                <FormControl fullWidth sx={{ m: 1 }}>
+                    <InputLabel id="category-select-label">Categories</InputLabel>
+                    <Select
+                        multiple
+                        value={formData.categories}
+                        onChange={handleSelectChange}
+                        input={<OutlinedInput label="Categories" />}
+                        renderValue={(selected) => selected.join(", ")}
+                        labelId="category-select-label"
+                    >
+                        {categories.map((category) => (
+                            <MenuItem value={category.name} key={category.name}>
+                                <Checkbox checked={formData.categories.indexOf(category.name) > -1} />
+                                <ListItemText primary={category.name} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
                 <br />
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        p: 1,
-                        m: 1,
-                        bgcolor: "background.paper",
-                        borderRadius: 1,
-                        justifyContent: "center",
-                    }}
-                >
-                    <Box style={{ margin: "10px 20px" }}>
-                        <TextField
-                            error={errors.username}
-                            helperText={errors.username ? "This field cannot be empty." : ""}
-                            label="Username"
-                            onChange={handleChange("username")}
-                        />
-                        <TextField label="Password (Optional)" type="password" name="password" />
-                    </Box>
-                </Box>
                 <Button color="warning" variant="contained" type="submit" sx={{ m: 1 }}>
                     Submit
                 </Button>

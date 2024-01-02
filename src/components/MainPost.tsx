ebe03367757eb55d4c7ef9866258a60156104560
slye20 <samuellim2011@gmail.com>
2024-01-02 16:57:20 +0800

    Functionality to delete threads

--- src/components/MainPost.tsx
+++ src/components/MainPost.tsx
@@ -24,7 +24,34 @@ const MainPost: React.FC<Prop> = ({ thread1 }) => {
         setEdit(true);
     };
 
-    const handleDelete = () => {};
+    const handleDelete = () => {
+        fetch(`http://localhost:3000/forum_threads/${threadId}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${localStorage.jwt}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ thread }),
+            credentials: "include",
+        })
+            .then((res) => {
+                if (res.ok) {
+                    navigate("/");
+                } else if (res.status === 401) {
+                    // 401 unauthorized
+                    navigate("/login");
+                } else if (res.status === 406 || res.status === 422) {
+                    // 406 not acceptable
+                    return res.json().then((err) => {
+                        console.log(err);
+                        throw new Error(err.error.join("\n"));
+                    });
+                } else {
+                    throw new Error("An error occurred. Please try again later.");
+                }
+            })
+            .catch((error) => setError(error.message));
+    };
 
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();

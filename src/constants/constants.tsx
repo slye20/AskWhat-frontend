f4a81ebc6603ec4692776194da298fee9ce0d098
slye20 <samuellim2011@gmail.com>
2024-01-19 10:54:00 +0800

    change api_url

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,4 @@
 // API Endpoints: URLs for your backend services.
 
 // export const API_URL = "http://localhost:3000"; // for local development
-export const API_URL = "https://askwhat-backend-195dc318e9d7.herokuapp.com";
+export const API_URL = "https://askwhat-backend-0ce5c40decb3.herokuapp.com";
